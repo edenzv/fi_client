8d2dbf56381683edfc4e1513c18936d9b6758849
Zvulun <m.zvulun@f5.com>
2019-03-24 13:28:01 +0200

    first commit

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,27 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../_models';
+import { apiUrl } from './authentication.service';
+
+export interface IUser {
+  userName: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  role: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+    private authApi = `${apiUrl}/api/user`;
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>(`${apiUrl}/users`);
+    }
+
+   create(user: IUser) {
+      return this.http.post(this.authApi + '/register', user);
+    }
+}
